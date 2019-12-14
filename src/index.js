33cbd12c296e08c180568af134cb63f326e8a508
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2019-12-15 00:12:54 +0300

    homework 4-1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default function convertTry(inpNum) {
   try {
-    if (!Number(inpNum)) {
+    if (inpNum.search(/[a-z,*+\\/\\]+/i) !== -1) {
       throw new Error('Введено не число');
     }
     return Number(inpNum);
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -6,25 +6,31 @@ test('Введено десятичные числа - 11', () => {
   expect(received).toBe(expected);
 });
 
-test('Введено двоичные числа - 0B11', () => {
-  const expected = 3;
+test('Введены не числа - 0B11', () => {
+  const expected = 'error';
   const received = convertTry('0B11');
   expect(received).toBe(expected);
 });
 
-test('Введено шестнадцатеричные числа - 0Xb', () => {
-  const expected = 11;
+test('Введены не числа - 0Xb', () => {
+  const expected = 'error';
   const received = convertTry('0Xb');
   expect(received).toBe(expected);
 });
 
-test('Обрабатывается ошибка 1+1', () => {
+test('Введены не числа - 1/1', () => {
+  const expected = 'error';
+  const received = convertTry('1/1');
+  expect(received).toBe(expected);
+});
+
+test('Введены не числа 1+1', () => {
   const expected = 'error';
   const received = convertTry('1+1');
   expect(received).toBe(expected);
 });
 
-test('Обрабатывается ошибка - 1dfsf4564654', () => {
+test('Введены не числа - 1dfsf4564654', () => {
   const expected = 'error';
   const received = convertTry('1dfsf4564654');
   expect(received).toBe(expected);
